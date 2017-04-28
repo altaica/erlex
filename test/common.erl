91961c59d94e3a77f2736694dc78c5449e4b0612
Phil Dempster <phil.dempster@cambridgeconsultants.com>
2017-04-28 16:59:13 +0100

    Track participants

--- test/common.erl
+++ test/common.erl
@@ -6,7 +6,7 @@ join(Server, Config) ->
     {_Prev, Conference} = get_saved_config(?config(saved_config, Config)),
     F = fun() ->
         {joined, Conference} = Server:join(),
-        loop(Server)
+        loop(Server, [self() | Conference])
     end,
     Pid = spawn(F),
     {save_config, [Pid | Conference]}.
@@ -14,16 +14,16 @@ join(Server, Config) ->
 get_saved_config(undefined) -> {undefined, []};
 get_saved_config(SavedConfig) -> SavedConfig.
 
-loop(Server) ->
+loop(Server, Conference) ->
     receive
         {connected, Pid} ->
             ok = Server:send({hello, Pid}),
-            loop(Server);
-        {diconnected, _Pid} ->
-            loop(Server);
+            loop(Server, [Pid | Conference]);
+        {diconnected, Pid} ->
+            loop(Server, lists:delete(Pid, Conference));
         {message, From, {hello, Pid}} when From =/= Pid, Pid =:= self() ->
             % Somebody has greeted us.
             ok;
         _Msg ->
-            loop(Server)
+            loop(Server, Conference)
     end.
