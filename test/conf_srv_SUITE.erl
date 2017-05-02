2531ba10160da7db4d840992b2ce31c52d37bd41
Phil Dempster <phil.dempster@cambridgeconsultants.com>
2017-05-02 16:31:39 +0100

    All leave

--- test/conf_srv_SUITE.erl
+++ test/conf_srv_SUITE.erl
@@ -20,20 +20,22 @@ instances() ->
     [conf_srv01, conf_srv02, conf_srv03].
 
 testcases() ->
-    [joe_joins, mike_joins, robert_joins, elephant].
+    [joe_joins, mike_joins, robert_joins, elephant, all_leave].
 
 joe_joins(Config) ->    join(Config).
 mike_joins(Config) ->   join(Config).
 robert_joins(Config) -> join(Config).
 elephant(Config) ->     join(Config).
-
+all_leave(Config) ->
+    {elephant, Conference} = get_saved_config(?config(saved_config, Config)),
+    lists:foreach(fun(Caller) -> Caller ! hup end, Conference).
 
 join(Config) ->
     Server = ?config(server, Config),
     {_Prev, Conference} = get_saved_config(?config(saved_config, Config)),
     F = fun() ->
         {joined, Conference} = Server:join(),
-        loop(Server, [self() | Conference])
+        ok = loop(Server, [self() | Conference])
     end,
     Pid = spawn(F),
     {save_config, [Pid | Conference]}.
@@ -44,13 +46,15 @@ get_saved_config(SavedConfig) -> SavedConfig.
 loop(Server, Conference) ->
     receive
         {connected, Pid} ->
+            % Send greeting to new caller.
             ok = Server:send({hello, Pid}),
             loop(Server, [Pid | Conference]);
         {diconnected, Pid} ->
             loop(Server, lists:delete(Pid, Conference));
         {message, From, {hello, Pid}} when From =/= Pid, Pid =:= self() ->
-            % Somebody has greeted us.
-            ok;
-        _Msg ->
-            loop(Server, Conference)
+            % Somebody else has greeted us.
+            loop(Server, Conference);
+        hup -> ok;
+        Msg ->
+            ct:fail({unexpected, Msg})
     end.
