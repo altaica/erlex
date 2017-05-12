d3671943c60c7ca48f988ff27c4851a126920da2
Phil Dempster <phil.dempster@cambridgeconsultants.com>
2017-05-12 09:15:42 +0100

    Revert; better is the enemy of good [enough].

--- test/coverage_check.sh
+++ test/coverage_check.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 set -e
-N=`grep -c "<td><strong>Total</strong></td><td>100%</td>" _build/test/cover/index.html`
-if [ $N -lt 3 ]; then
-    echo "Coverage check failed: $N out of 3 modules have full test coverage"
-    exit 1
-fi
+grep -q "<td><strong>Total</strong></td><td>100%</td>" _build/test/cover/index.html
