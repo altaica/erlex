c991bfc4893eb4cbe12e60999cdde3b36bb7dc83
Phil Dempster <phil.dempster@cambridgeconsultants.com>
2017-05-12 09:03:46 +0100

    Improve coverage_check script NB a better way would be to have an escript

--- test/coverage_check.sh
+++ test/coverage_check.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 set -e
-grep -q "<td><strong>Total</strong></td><td>100%</td>" _build/test/cover/index.html
+N=`grep -c "<td><strong>Total</strong></td><td>100%</td>" _build/test/cover/index.html`
+if [ $N -lt 3 ]; then
+    echo "Coverage check failed: $N out of 3 modules have full test coverage"
+    exit 1
+fi
