2894b8d081f438b526a806c3ce0484731b9d5307
Phil Dempster <phil.dempster@cambridgeconsultants.com>
2017-05-12 10:50:23 +0100

    Perform check in terminate callback

--- test/client_bot.erl
+++ test/client_bot.erl
@@ -1,4 +1,9 @@
 %%% @doc OTP gen_server implementation of client bot.
+%%%
+%%% When the server informs the bot that another caller has joined,
+%%% the bot will send a greeting {hello, <Pid>} to the new caller.
+%%%
+%%% @end
 %%% @copyright 2017 Phil Dempster
 
 -module(client_bot).
@@ -9,15 +14,17 @@
 
 %%% API
 
+%% @doc Start a call with the specified server.
+%% @returns Tuple containing the Pid of the client bot and a list of the current conference participants.
 -spec start(module()) -> {pid(), list(pid())}.
 start(Server) ->
     {ok, Pid} = gen_server:start(?MODULE, [Server], []),
-    {_Server, _Expected, InitState} = gen_server:call(Pid, get_state),
-    {Pid, InitState}.
+    {_Server, _Expected, Participants} = gen_server:call(Pid, get_state),
+    {Pid, Participants}.
 
+%% @doc Terminate our call.
 -spec stop(pid()) -> ok.
 stop(Pid) ->
-    {_Server, [], _InitState} = gen_server:call(Pid, get_state),
     gen_server:stop(Pid).
 
 
@@ -33,22 +40,23 @@ handle_call(get_state, _From, State) ->
 handle_cast(_Msg, State) ->
     {stop, undefined, State}.
 
-handle_info({connected, Pid}, {Server, _Expected, _InitState} = State) ->
+handle_info({connected, Pid}, {Server, _Expected, _Participants} = State) ->
     % Send greeting to new caller.
     ok = Server:send({hello, Pid}),
     {noreply, State};
 handle_info({disconnected, _Pid}, State) ->
     % Pid should not be in expected list.
     {noreply, State};
-handle_info({message, From, {hello, Pid}}, {Server, Expected, InitState})
+handle_info({message, From, {hello, Pid}}, {Server, Expected, Participants})
         when From =/= Pid, Pid =:= self() ->
     % Somebody else has greeted us. Remove from the expected list.
-    {noreply, {Server, lists:delete(From, Expected), InitState}};
+    {noreply, {Server, lists:delete(From, Expected), Participants}};
 handle_info({message, _From, {hello, _Pid}}, State) ->
     {noreply, State}.
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-terminate(_Reason, _State) ->
+terminate(_Reason, {_Server, [], _Participants}) ->
+    % Verify that we have been greeted by all the original participants.
     ok.
