6bd3c91389d0a44eece9715a69b76bee1dfe2148
phil <git@altaica.co.uk>
2017-07-17 15:05:49 +0100

    Configurable timings

--- apps/traffic_light/test/traffic_light02_test.erl
+++ apps/traffic_light/test/traffic_light02_test.erl
@@ -3,24 +3,30 @@
 
 -module(traffic_light02_test).
 -include_lib("eunit/include/eunit.hrl").
+-define(TIMINGS, #{
+    green       => 20000,
+    amber       => 3000,
+    red         => 30000,
+    redamber    => 2000
+}).
 
 transition_from_green_test() ->
     % Duration specified by Highways Agency.
-    ?assertMatch({next_state, amber, {100, 100}, {state_timeout, 3000, transition}},
-        traffic_light02:handle_event(state_timeout, transition, green, {100, 100})).
+    ?assertMatch({next_state, amber, _Timings, {state_timeout, 3000, transition}},
+        traffic_light02:handle_event(state_timeout, transition, green, ?TIMINGS)).
 
 transition_from_amber_test() ->
-    ?assertMatch({next_state, red, {100, 100}, {state_timeout, _Period, transition}},
-        traffic_light02:handle_event(state_timeout, transition, amber, {100, 100})).
+    ?assertMatch({next_state, red, _Timings, {state_timeout, _Period, transition}},
+        traffic_light02:handle_event(state_timeout, transition, amber, ?TIMINGS)).
 
 transition_from_red_test() ->
     % Duration specified by Highways Agency.
-    ?assertMatch({next_state, redamber, {100, 100}, {state_timeout, 2000, transition}},
-        traffic_light02:handle_event(state_timeout, transition, red, {100, 100})).
+    ?assertMatch({next_state, redamber, _Timings, {state_timeout, 2000, transition}},
+        traffic_light02:handle_event(state_timeout, transition, red, ?TIMINGS)).
 
 transition_from_redamber_test() ->
-    ?assertMatch({next_state, green, {100, 100}, {state_timeout, _Period, transition}},
-        traffic_light02:handle_event(state_timeout, transition, redamber, {100, 100})).
+    ?assertMatch({next_state, green, _Timings, {state_timeout, _Period, transition}},
+        traffic_light02:handle_event(state_timeout, transition, redamber, ?TIMINGS)).
 
 start_test() ->
     {ok, Pid} = traffic_light02:start(),
