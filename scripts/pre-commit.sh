b0bcb6779dee4f9f67988c52e9f6dee79a0e944a
phil <git@altaica.co.uk>
2017-08-08 15:33:51 +0000

    Fix script calling

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -5,10 +5,8 @@ set -e
 
 rm -rf _build
 
-rebar3 do version, tree, xref
-time rebar3 dialyzer
-time rebar3 do eunit, ct, cover
-escript scripts/elvis -Vv --config scripts/elvis.config rock
+rebar3 do dialyzer, xref, eunit, ct, cover
+rebar3 as lint lint
 sh scripts/coverage_check.sh
 
 rebar3 as prod do version, tree, tar
