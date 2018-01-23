19f7b4351e37c6a057b05647f87038f0884644fe
phil <phil@altaica.co.uk>
2018-01-23 09:56:05 +0000

    Comments & layout

--- apps/hello_joe/src/chatbot.erl
+++ apps/hello_joe/src/chatbot.erl
@@ -1,39 +1,49 @@
+%%%---------------------------------------------------------------------
 %%% @copyright 2017 Phil Dempster
 %%%
 %%% @doc OTP gen_server implementation of client bot.
 %%%
 %%% When the server informs the bot that another caller has joined,
-%%% the bot will send a greeting <code>{hello, Pid}</code> to the new caller.
+%%% the bot will send a greeting <code>{hello, Pid}</code> to the new
+%%% caller.
 %%%
 %%% The bot expects that calls will be torn down in the same order
 %%% as they are set up. This allows for disconnect testing.
 %%%
 %%% @end
+%%%---------------------------------------------------------------------
 
 -module(chatbot).
 -behaviour(gen_server).
+
 -export([start/2, stop/1]).
+
 -export([init/1,
          handle_call/3,
          handle_cast/2,
          handle_info/2,
          terminate/2]).
 
+
 -type calls() :: [pid()].
 
 -record(state, {
-    server ::       module(),
-    expected ::     calls(),
-    init_calls ::   calls(),
-    bot_name ::     atom()
+    server :: module(),
+    expected :: calls(),
+    init_calls :: calls(),
+    bot_name :: atom()
 }).
 
 -type state() :: #state{}.
 
+
+%%%---------------------------------------------------------------------
 %%% API
+%%%---------------------------------------------------------------------
 
 %% @doc Start a call with the specified server,
-%%      returning the Pid of the client bot and a list of the current conference participants.
+%%      returning the Pid of the client bot and a list of the current
+%%      conference participants.
 %% @end
 
 -spec start(Module, Name) -> T when
@@ -58,7 +68,9 @@ stop(Pid) ->
     gen_server:stop(Pid).
 
 
-%%% Implementation
+%%%---------------------------------------------------------------------
+%%% OTP callbacks
+%%%---------------------------------------------------------------------
 
 %% @doc Join the conference server.
 
@@ -115,11 +127,10 @@ handle_info({connected, Pid}, #state{server = Server} = State) ->
     {noreply, State};
 handle_info({disconnected, Pid}, #state{init_calls = [Pid | Calls]} = State) ->
     {noreply, State#state{init_calls = Calls}};
-handle_info({message, From, {hello, Name}},
-            #state{expected = Expected, bot_name = BotName} = State)
-        when From =/= self(), Name =:= BotName ->
-    error_logger:info_msg("~w: ~w greeted us~n", [BotName, From]),
-    {noreply, State#state{expected = lists:delete(From, Expected)}};
+handle_info({message, From, {hello, Name}}, State)
+        when From =/= self(), Name =:= State#state.bot_name ->
+    error_logger:info_msg("~w: ~w greeted us~n", [State#state.bot_name, From]),
+    {noreply, State#state{expected = lists:delete(From, State#state.expected)}};
 handle_info({message, _From, {hello, _Name}}, State) ->
     {noreply, State}.
 
@@ -131,6 +142,10 @@ handle_info({message, _From, {hello, _Name}}, State) ->
 terminate(_Reason, #state{expected = [], init_calls = []}) ->
     ok.
 
+
+%%%---------------------------------------------------------------------
+%%%
+%%%---------------------------------------------------------------------
 
 lookup(Calls) ->
     [pid_to_name(Pid) || Pid <- Calls].
