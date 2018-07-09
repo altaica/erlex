245c07482bd0e6cf410e86c99ab0615857c2550c
Phil Dempster <phil.dempster@cambridgeconsultants.com>
2018-07-09 11:18:11 +0100

    More typos

--- scripts/tidy_check.sh
+++ scripts/tidy_check.sh
@@ -2,6 +2,6 @@
 # Run erl_tidy tool in test mode (with some filters)
 
 set -e
-erl -noshell -eval "erl_tidy:dir("apps", [{test, true}])." -s init stop \
+erl -noshell -eval "erl_tidy:dir(\"apps\", [{test, true}])." -s init stop \
     | grep -v "tidying directory" \
-    | grep -v "apps/[a-z0-9_]*/test""
+    | grep -v "apps/[a-z0-9_]*/test"
